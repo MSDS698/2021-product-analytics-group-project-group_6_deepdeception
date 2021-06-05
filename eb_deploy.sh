6ca094af75ff20960bc6ec4dbe37eefd3c4a9c49
MauroDNapoli <maurodanielnapoli@gmail.com>
2021-06-05 14:33:31 -0700

    add latests sphinx

--- eb_deploy.sh
+++ eb_deploy.sh
@@ -1,2 +1,2 @@
-eb init deception_perception --region us-west-2 --platform Docker
-eb create deception_perception-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb init week5 --region us-west-2 --platform Docker
+eb create week5-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
