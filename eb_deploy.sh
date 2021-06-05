b8c29bb83da981861baaa06b9790384d43d787d7
MauroDNapoli <maurodanielnapoli@gmail.com>
2021-06-05 12:48:32 -0700

    change env variables

--- eb_deploy.sh
+++ eb_deploy.sh
@@ -1,2 +1,2 @@
-eb init week2 --region us-west-2 --platform Docker
-eb create week2-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+eb init deception_perception --region us-west-2 --platform Docker
+eb create deception_perception-env --verbose --envvars AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --envvars AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
