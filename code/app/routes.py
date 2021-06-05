a9652f3371f985ea9d55ccbfe577be7762f512f6
MauroDNapoli <maurodanielnapoli@gmail.com>
2021-06-05 12:46:05 -0700

    remove routes.py file

--- code/app/routes.py
+++
@@ -1,9 +0,0 @@
-from app import application
-from flask import render_template
-
-
-@application.route('/')
-@application.route('/index')
-def index():
-    return("<h1> Hello World </h1>")
-    #return (render_template('index.html', author='Diane Woodbridge'))
