861cae209daaebb622434c16671ed348d80c69ef
MauroDNapoli <maurodanielnapoli@gmail.com>
2021-06-08 15:41:11 -0700

    add new routes and requirements

--- code/app/__init__.py
+++ code/app/__init__.py
@@ -1,9 +1,22 @@
-from flask import Flask
 import os
+
+from config import Config
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 # Initialization
 # Create an application instance (an object of class Flask)  which handles all requests.
 application = Flask(__name__)
-application.secret_key = os.urandom(24)
+application.config.from_object(Config)
+db = SQLAlchemy(application)
+db.create_all()
+db.session.commit()
 
-from app import routes_2 # routes.py needs to import "application" variable in __init__.py (Altough it violates PEP8 standards)
+# login_manager needs to be initiated before running the app
+login_manager = LoginManager()
+login_manager.init_app(application)
+
+# Added at the bottom to avoid circular dependencies. (Altough it violates PEP8 standards)
+from app import classes
+from app import routes_2
