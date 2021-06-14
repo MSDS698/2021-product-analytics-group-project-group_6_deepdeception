c04c0c1ae82f636be121cc28bd96c22efb1e9aab
MauroDNapoli <maurodanielnapoli@gmail.com>
2021-06-13 19:23:57 -0700

    change db name

--- code/config.py
+++ code/config.py
@@ -5,5 +5,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'week3.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'deception_perception.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = True  # flask-login uses sessions which require a secret Key
