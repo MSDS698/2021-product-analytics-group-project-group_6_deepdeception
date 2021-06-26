2c088b84798ea4d5be96254531b0a1ca446f8b6b
erifuna1999 <erikofunasato@gmail.com>
2021-06-26 10:50:41 -0700

    updated unit_test

--- code/unit_test.py
+++ code/unit_test.py
@@ -3,8 +3,12 @@ import os
 import torch
 import numpy as np
 from app.routes_2 import *
+from app.classes import *
 from app import model
 from app import tokenizer
+from flask.testing import FlaskClient
+import pytest
+
 
 def test_download_model():
     """
@@ -39,3 +43,60 @@ def test_model():
     preds = soft_preds.detach().numpy()[0]
     assert all(np.isclose(actual_values, preds, atol=1e-4))
     assert hard_preds[0] == 1
+
+@pytest.fixture(scope='module')
+def flask_app():
+    app = application
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture(scope='module')
+def client(flask_app):
+    """client - the test client from the application's text request context"""
+    app = flask_app
+    ctx = flask_app.test_request_context()
+    ctx.push()
+    app.test_client_class = FlaskClient
+    return app.test_client()
+
+
+def test_login_required(client):
+    """
+    Input: client - the test client from the application's text request context
+    unit test: Test login is required:
+        1. To access to predict page.
+        2. After register for an account.
+    """
+    res = client.get('/predict')
+    assert b'login' in res.data
+
+    with client:
+        res = client.post('/register', data=dict(username='admin',
+                                                 email='test@gmail.com',
+                                                 password='test'),
+                          follow_redirects=True)
+        assert b'login' in res.data
+
+def test_login_to_predict(client):
+    """
+    Input: client - the test client from the application's text request context
+    unit test: Test if a user is able to access the prediction page after being logged in.
+    """
+    with client:
+        res = client.post('/login', data=dict(username='admin', password='test'),
+                          follow_redirects=True)
+        # res = client.get('/predict')
+        assert b'Deception' in res.data
+
+def test_logout(client):
+    """
+    Input: client - the test client from the application's text request context
+    unit test: Test login is required after a user being logged out to access prediction
+    """
+    with client:
+        client.post('/login', data=dict(username='admin', password='test'),
+                    follow_redirects=True)
+        res = client.get('/logout', follow_redirects=True)
+        assert b'login' in res.data
+
